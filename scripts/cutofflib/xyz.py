89b9048703c6da7bd4b9f69f4dc61cfa1695528a
hnmr293 <hnmr293@gmail.com>
2023-03-23 00:47:20 +0900

    fix xyz_grid issue

--- scripts/cutofflib/xyz.py
+++ scripts/cutofflib/xyz.py
@@ -35,6 +35,38 @@ def to_bool(v: str):
         s = ', '.join([f'`{v}`' for v in acceptable])
         raise ValueError(f'value must be one of {s}.')
 
+
+class AxisOptions:
+    
+    def __init__(self, AxisOption: type, axis_options: list):
+        self.AxisOption = AxisOption
+        self.target = axis_options
+        self.options = []
+    
+    def __enter__(self):
+        self.options.clear()
+        return self
+    
+    def __exit__(self, ex_type, ex_value, trace):
+        if ex_type is not None:
+            return
+        
+        for opt in self.options:
+            self.target.append(opt)
+        
+        self.options.clear()
+    
+    def create(self, name: str, type_fn: Callable, action: Callable, choices: Union[List[str],None]):
+        if choices is None or len(choices) == 0:
+            opt = self.AxisOption(name, type_fn, action)
+        else:
+            opt = self.AxisOption(name, type_fn, action, choices=lambda: choices)
+        return opt
+    
+    def add(self, axis_option):
+        self.target.append(axis_option)
+
+
 __init = False
 
 def init_xyz(script: type, ext_name: str):
@@ -45,23 +77,50 @@ def init_xyz(script: type, ext_name: str):
     
     for data in scripts.scripts_data:
         name = os.path.basename(data.path)
-        if name == 'xy_grid.py' or name == 'xyz_grid.py':
-            AxisOption = data.module.AxisOption
-            
-            def define(param: str, index: int, type: Callable, choices: List[str] = []):
-                def fn(p, x, xs):
-                    __set_value(p, script, index, x)
-                if len(choices) == 0:
-                    data.module.axis_options.append(AxisOption(f'{ext_name} {param}', type, fn))
-                else:
-                    data.module.axis_options.append(AxisOption(f'{ext_name} {param}', type, fn, choices=lambda: choices))
-            
-            define('Enabled', 0, to_bool, choices=['false', 'true'])
-            define('Targets', 1, str)
-            define('Weight', 2, float)
-            define('Disable for Negative Prompt', 3, to_bool, choices=['false', 'true'])
-            define('Strong', 4, to_bool, choices=['false', 'true'])
-            define('Padding', 5, str)
-            define('Interpolation', 6, str, choices=['Lerp', 'SLerp'])
+        if name != 'xy_grid.py' and name != 'xyz_grid.py':
+            continue
+        
+        if not hasattr(data.module, 'AxisOption'):
+            continue
+        
+        if not hasattr(data.module, 'axis_options'):
+            continue
+        
+        AxisOption = data.module.AxisOption
+        axis_options = data.module.axis_options
+        
+        if not isinstance(AxisOption, type):
+            continue
+        
+        if not isinstance(axis_options, list):
+            continue
+        
+        try:
+            create_options(ext_name, script, AxisOption, axis_options)
+        except:
+            pass
             
     __init = True
+
+
+def create_options(ext_name: str, script: type, AxisOptionClass: type, axis_options: list):
+    with AxisOptions(AxisOptionClass, axis_options) as opts:
+        def define(param: str, index: int, type_fn: Callable, choices: List[str] = []):
+            def fn(p, x, xs):
+                __set_value(p, script, index, x)
+            
+            name = f'{ext_name} {param}'
+            return opts.create(name, type_fn, fn, choices)
+        
+        options = [
+            define('Enabled', 0, to_bool, choices=['false', 'true']),
+            define('Targets', 1, str),
+            define('Weight', 2, float),
+            define('Disable for Negative Prompt', 3, to_bool, choices=['false', 'true']),
+            define('Strong', 4, to_bool, choices=['false', 'true']),
+            define('Padding', 5, str),
+            define('Interpolation', 6, str, choices=['Lerp', 'SLerp']),
+        ]
+        
+        for opt in options:
+            opts.add(opt)
